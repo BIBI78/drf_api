73fd48e1d98becfc43715081cebcfca818a56f09
BIBI78 <blakecollins78@gmail.com>
2024-01-16 11:29:39 +0000

    DEBUGGING : debugging seriializer for mp3, deleting get id owner

--- mp3/serializers.py
+++ mp3/serializers.py
@@ -13,8 +13,7 @@ class Mp3Serializer(serializers.ModelSerializer):
     comments_count = serializers.ReadOnlyField()
 
     def create(self, validated_data):
-        mp3_file = validated_data.get('mp3')  # Use the correct field name
-
+        mp3 = validated_data.get('mp3')  
         # Cloudinary integration code
         cloudinary_options = {
             'resource_type': 'auto',
@@ -22,7 +21,7 @@ class Mp3Serializer(serializers.ModelSerializer):
         }
 
         try:
-            result = upload(mp3_file, **cloudinary_options)
+            result = upload(mp3, **cloudinary_options)
             validated_data['mp3'] = result['secure_url']
         except Exception as e:
             # Handle the exception (e.g., raise a serializers.ValidationError)
@@ -35,20 +34,11 @@ class Mp3Serializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
-    def get_like_id(self, obj):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            like = Like.objects.filter(
-                owner=user, post=obj
-            ).first()
-            return like.id if like else None
-        return None
-
     class Meta:
         model = Mp3
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'mp3',
-            'like_id', 'likes_count', 'comments_count',
+             'like_id', 'likes_count', 'comments_count',
         ]
