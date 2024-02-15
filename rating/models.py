adcaa94e6fb9dd368edb15e2309a2dc68e1da72d
BIBI78 <blakecollins78@gmail.com>
2024-02-15 14:59:31 +0000

    UPDATE: update the models py in rating app

--- rating/models.py
+++ rating/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.models import Post
+
+from beats.models import Beat
 
 
 class Rating(models.Model):
@@ -8,16 +9,13 @@ class Rating(models.Model):
     Rating model with related name ratings
     """
     rating = models.IntegerField()
-    # event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='ratings')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    #  new bs
-    post = models.ForeignKey(
-        Post, related_name='ratings', on_delete=models.CASCADE, blank=True, null=True
+    beat = models.ForeignKey(
+        Beat, related_name='ratings', on_delete=models.CASCADE, blank=True, null=True
     )
-   
-   
+
     
     class Meta:
         ordering = ['-created_at']
