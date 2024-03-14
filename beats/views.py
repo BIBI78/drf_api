12fd9a257040d103c70920d02925cca6ed2f898f
BIBI78 <blakecollins78@gmail.com>
2024-03-14 14:38:42 +0000

    UPDATE: update the beats views for feedback info , not sure if it will work on the front now

--- beats/views.py
+++ beats/views.py
@@ -1,10 +1,10 @@
-from django.db.models import Count
+from django.db.models import Count, Case, When, BooleanField
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Beat
 from .serializers import BeatSerializer
-
+from feedback.models import Feedback  # Assuming the feedback app name is 'feedback'
 
 class BeatList(generics.ListCreateAPIView):
     """
@@ -14,7 +14,12 @@ class BeatList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Beat.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)  
+        comments_count=Count('comment', distinct=True),
+        fire_count=Count(Case(When(feedbacks__fire=True, then=1), output_field=BooleanField())),
+        cold_count=Count(Case(When(feedbacks__cold=True, then=1), output_field=BooleanField())),
+        hard_count=Count(Case(When(feedbacks__hard=True, then=1), output_field=BooleanField())),
+        trash_count=Count(Case(When(feedbacks__trash=True, then=1), output_field=BooleanField())),
+        loop_count=Count(Case(When(feedbacks__loop=True, then=1), output_field=BooleanField())),
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
