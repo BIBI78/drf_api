048699e105f92027201fe8c52fd000533b2cac14
BIBI78 <blakecollins78@gmail.com>
2024-04-03 09:23:51 +0000

    DEBUGGING : debugging profiles serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
-
+from beats.models import Beat  # Import the Beat model
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-    beats_count = serializers.ReadOnlyField()
+    beats_count = serializers.SerializerMethodField()  # Change to SerializerMethodField
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
@@ -21,9 +21,11 @@ class ProfileSerializer(serializers.ModelSerializer):
             following = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
-            # print(following)
             return following.id if following else None
         return None
+
+    def get_beats_count(self, obj):  # Define method to get beats count
+        return Beat.objects.filter(owner=obj.owner).count()
 
     class Meta:
         model = Profile
