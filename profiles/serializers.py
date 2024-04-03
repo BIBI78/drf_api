5bdb56b394d183ac725771661149eb608aafcd99
BIBI78 <blakecollins78@gmail.com>
2024-04-03 09:11:24 +0000

    UPDATE: update profiles object to contain beats_count

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,7 +7,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-    posts_count = serializers.ReadOnlyField()
+    beats_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
@@ -30,5 +30,5 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'following_id',
-            'posts_count', 'followers_count', 'following_count',
+            'beats_count', 'followers_count', 'following_count',
         ]
